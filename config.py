434db57fb35be6774022740a1ad67928515545e4
Lucky44457 <maribelwate@gmail.com>
2025-07-25 11:48:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7999460846:AAG9N2_b4kV2_V4MgxJIZLGeFlOClKFH89M")
-API_ID = int(os.environ.get("API_ID", "29736812"))
-API_HASH = os.environ.get("API_HASH", "d22c2fc9b388eef56e456f546b1d9c1a")
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002373501691"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7999460AAG9N2_b42_V4MgxJIZLGeFlOClKFH89M")
+API_ID = int(os.environ.get("API_ID", "29812"))
+API_HASH = os.environ.get("API_HASH", "d22c2fc9b388ee45646b1d9c1a")
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002301691"))
 AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "6197171929").split())
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://jackend44:1CamIKdwrLDXuhns@cluster0.nlnbwph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "BroadcastBot")
